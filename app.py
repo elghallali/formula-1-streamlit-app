1c32d263185a693f68e30c8c5650488daf8af515
Yassine EL GHALLALI <y.elghallali@gmail.com>
2023-11-04 02:51:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import os
+import glob
+import io
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -11,12 +13,24 @@ st.set_page_config(
     layout='wide'
 )
 
+csv_files = glob.glob('data/*.csv')
+
 st.title(':bar_chart: Formula 1 EDA')
 st.markdown('<style> div.block-container {padding-top: 1rem;}</style>',unsafe_allow_html=True)
 
-fl = st.file_uploader(':file_folder: Upload a file', type=['csv','txt','xlsx','xls'])
-
-if fl is not None:
-    filename = fl.name
-    st.write(filename)
-    df = pd.read_csv(filename)
+for file in csv_files:
+    df = pd.read_csv(file, index_col=0)
+    title = file.split('\\')[1].split('.')[0].replace('_',' ').capitalize()
+    st.markdown(f"## {title}" )
+    tab1, tab2, tab3, tab4 = st.tabs([":card_file_box: Data", "Types", 'NAN', 'Info'])
+    tab1.subheader("A tab with a data")
+    tab1.dataframe(df)
+    tab2.subheader("A tab with a Columns type")
+    tab2.dataframe(df.dtypes)
+    tab3.subheader("A tab with an info")
+    tab3.dataframe(df.isna().sum())
+    tab4.subheader('A tab with an Info')
+    buffer = io.StringIO()
+    df.info(buf=buffer)
+    s = buffer.getvalue()
+    tab4.text(s)
